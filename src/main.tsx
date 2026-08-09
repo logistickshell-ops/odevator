c1dfc538c3504830cf3b7405e6d2136784a3ebd5
Disa_ad <logistickshell@gmail.com>
2026-08-09 17:35:51 +0300

    Update main.tsx
    коп

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Root element not found!");
-}
-
-try {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-  console.log("App rendered successfully!");
-} catch (error) {
-  console.error("Failed to render app:", error);
-  rootElement.innerHTML = '<div style="padding: 20px; text-align: center;">Ошибка загрузки приложения. Пожалуйста, обновите страницу.</div>';
-}
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
